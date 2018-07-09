4f9ff26d7f1766e8885611a0864651d895ba231f
AETERNAM <victor.martinez@alphacredit.mx>
2018-07-09 00:03:46 -0500

    fixing update model

--- src/form-generator.vue
+++ src/form-generator.vue
@@ -159,7 +159,7 @@
             },
             onInput: function(value, fieldName) {
                 this.$set(this.model, fieldName, value)
-                this.$emit("update:model", this.model)
+                this.$emit("update_model", this.model)
                 this.resetForm()
             },
             resetForm: function() {
